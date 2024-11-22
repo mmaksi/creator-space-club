51874f4be15d4ad5d9133e403481fee5244bc27d
Mark Maksi <mmaksi.dev@gmail.com>
2024-11-22 06:17:38 +0100

    added a logger middleware

--- src/app.ts
+++ src/app.ts
@@ -2,22 +2,28 @@ import express from 'express';
 import 'express-async-errors';
 import api from './routes/api';
 import cookieSession from 'cookie-session';
+import { errorLogger } from 'express-winston';
+import { errorHandler } from './middlewares/error-handler';
+import { requestLogger } from './middlewares/logger';
+import helmet from 'helmet';
+import cors from 'cors';
+import hpp from 'hpp';
 
 export const app = express();
 
-// Important middlewares
+app.use(requestLogger);
+app.use(helmet());
+app.use(hpp());
+app.use(cors());
 app.set('trust proxy', 1);
 app.use(express.json());
 app.use(
     cookieSession({
+        httpOnly: true,
         signed: false,
         secure: process.env.NODE_ENV !== 'test',
     })
 );
-// applySecurityMiddlewares(app);
-
-// Router mounting
 app.use('/api', api);
-
-// Error handling
-// app.use(errorHandler);
+app.use(errorHandler);
+app.use(errorLogger);
